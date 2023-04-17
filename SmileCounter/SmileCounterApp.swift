5fcb2a2e2cd89da7c6c92f31dddcd8451a0a74e8
Joseph Samonte <josephisnt@hotmail.com>
2023-04-16 20:51:33 -0500

    Initial Commit

---
+++ SmileCounter/SmileCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmileCounterApp.swift
+//  SmileCounter
+//
+//  Created by Joseph Samonte on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SmileCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
