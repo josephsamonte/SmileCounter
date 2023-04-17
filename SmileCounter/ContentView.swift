6ef4c7e2401fd3f0286f4c844c7a3d0f431812e1
Joseph Samonte <josephisnt@hotmail.com>
2023-04-16 21:45:27 -0500

    My first commit to test smile iOS app

--- SmileCounter/ContentView.swift
+++ SmileCounter/ContentView.swift
@@ -7,15 +7,29 @@
 
 import SwiftUI
 
+enum Emoji: String, CaseIterable {
+    case 😀,😄,😁,😂
+}
+
 struct ContentView: View {
+    @State var selection: Emoji = .😂
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases, id: \.self) {emoji in
+                        Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("How big did you smile?")
+            .padding()
         }
-        .padding()
     }
 }
 
